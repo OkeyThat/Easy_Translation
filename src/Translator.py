35460c7880b6d67a4eaf037039f8df0cf1f5f03d
Heehang Ha <heehang523@gmail.com>
2023-01-05 23:33:28 +0900

    [제목] - copy 버튼 생성 [원인] - 자동으로 번역 한 뒤 카피하는 버튼 생성 필요 [개선] - 푸쉬 버튼 생성 [동작] -

--- src/Translator.py
+++ src/Translator.py
@@ -1,22 +1,26 @@
-
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
 
 class MyApp(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+  def __init__(self):
+      super().__init__()
+      self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
+  def initUI(self):
+      btn = QPushButton('Copy', self)
+      btn.move(50, 50)
+      btn.resize(btn.sizeHint())
+      btn.clicked.connect(QCoreApplication.instance().quit)
+
+      self.setWindowTitle('Auto Translation')
+      self.setGeometry(300, 300, 300, 200)
+      self.show()
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
